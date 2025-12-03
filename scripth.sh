caa8448277e6077808ffea3a1e57c65afc826fc0
Draw10p <daromanag@uide.edu.ec>
2025-12-03 15:41:44 +0000

    Trabajo en Clase 3 - DIC - 2025

---
+++ scripth.sh
@@ -0,0 +1,27 @@
+echo "Hola Linux" > archivo.txt
+
+# echo → Comando que imprime texto en la salida estándar.
+# "Hola Linux" → Texto que se va a imprimir.
+# > → Redirección, envía la salida al archivo.
+# archivo.txt → Nombre del archivo donde se guarda el texto.
+
+cat archivo.txt
+
+# cat → Muestra el contenido de un archivo.
+# archivo.txt → Archivo que se va a mostrar.
+
+cp archivo.txt copia.txt
+
+# cp → Copia archivos.
+# archivo.txt → Archivo origen.
+# copia.txt → Archivo destino.
+
+ls -l
+
+# ls → Lista archivos en el directorio.
+# -l → Opción para mostrar detalles (permisos, tamaño, fecha).
+
+rm archivo.txt
+
+# rm → Elimina archivos.
+# archivo.txt → Archivo que se va a borrar.
