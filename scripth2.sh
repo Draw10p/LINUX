8e1406d3d55070e2037c5dfea72346e0720e627b
Draw10p <daromanag@uide.edu.ec>
2025-12-03 15:46:29 +0000

    Trabajo en Clase.2 3 -DIC-2025

--- scripth2.sh
+++ scripth2.sh
@@ -1,4 +1,19 @@
 uname -a 
+
+# uname → Muestra información del sistema operativo y kernel.
+# -a → Opción para mostrar todos los detalles: nombre del kernel, versión, arquitectura, etc.
+
 lscpu
+
+# ls → Lista información (en este caso, no de archivos sino del CPU).
+# cpu → Indica que se debe mostrar detalles del procesador: núcleos, arquitectura, velocidad.
+
 free -h
-df
+
+# free → Muestra el uso de memoria RAM y swap.
+# -h → Formato “humano”, convierte bytes en KB, MB, GB para fácil lectura.
+
+df -h
+
+# df → Muestra el uso del espacio en disco por sistema de archivos.
+# -h → Formato “humano”, muestra tamaños en KB, MB, GB.
